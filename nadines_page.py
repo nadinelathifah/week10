905281a5a3c2ef9ac9f379c8a02db2e125afe548
Nadine Lathifah <nadinehendronoto@gmail.com>
2025-03-24 20:34:05 +0300

    CREATED: static, template and nadines_page

---
+++ nadines_page.py
@@ -0,0 +1,11 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/nadine/hello_nadine")
+def hello_nadine():
+    return render_template('nadine.html')
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
