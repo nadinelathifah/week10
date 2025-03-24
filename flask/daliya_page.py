db321951dfe82de85e2983b1ce677853e23e0f15
Daliya Stanley <daliya.kurien@gmail.com>
2025-03-24 18:22:52 +0000

    Added Daliya's files

---
+++ flask/daliya_page.py
@@ -0,0 +1,53 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/daliya')
+def hello_from_daliya():
+    return render_template("index1.html")
+
+@app.route("/daliya/hello1")
+def hello_1():
+    return render_template("index.html")
+
+# @app.route("/daliya/hello3")
+# def hello_3():
+#     return render_template("style.css")
+
+@app.route("/daliya/hello2")
+def hello_2():
+    return render_template("index.html", content="Testing")
+
+@app.route("/daliya/hello3")
+def hello_3():
+    return render_template("index2.html")
+
+
+
+# @app.route('/<string:name>')
+# def home(name):
+#     return """
+#      <html>
+#         <head>
+#         <title>Simple Flask App </title>
+#          </head>
+#
+#         <body>
+#             <h1>
+#                 Homepage
+#             </h1>
+#             <p>
+#                 Hello {}!
+#             </p>
+#
+#         </body>
+#      </html>
+#
+#
+#      """.format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
